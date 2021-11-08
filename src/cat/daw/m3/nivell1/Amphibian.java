bc353f1ba6a38d3d17af75f4f5a1e7236b462075
Marcello Krell <mkrell20@ilg.cat>
2021-11-08 03:53:35 +0000

    Marcello Krell: Primer nivell acabat

--- src/cat/daw/m3/nivell1/Amphibian.java
+++ src/cat/daw/m3/nivell1/Amphibian.java
@@ -13,4 +13,20 @@ public class Amphibian {
     public static void Frog() {
     }
     
+    public void jump(int jumps){
+        try{ 
+            if (jumps > 3) {
+                throw new Exception("The frog cant jump more than three times");
+            }
+            else {
+                System.out.println("The frog has jumped " + jumps + (jumps == 1 ? " times" : " times") + " !!");
+            }
+  
+        }
+
+        catch (Exception e){
+            System.out.println(e);
+        }
+
+    }
 }
